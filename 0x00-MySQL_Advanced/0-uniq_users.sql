db75a5f2c12117d3073615092ee4b98856c01b59
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-03-21 22:27:54 +0000

    Task complete

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- Creates a table users with below attributes
 -- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
--- If table exists, script will not fail, can be executed on any database
+-- email, string (255 chars), never null and unique.
+-- name, string (255 chars).
+-- Script will not fail If table exists, can be executed on any database.
 
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
